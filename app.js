0a5551d1f6433ca43cab23d090dcb44ffcaf6d0f
ashleyromano <ashleye.romano@gmail.com>
2022-03-26 19:07:45 -0400

    ?

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
 
-app.use(express.json());
+
+// var express = require("express");
+// var app = express();
 
 
 
-app.get('/recipes', (request, response) => {
-    response.json(recipes);
-});
+// app.use(express.json());
 
-app.get('/recipes/:id', (request, response) => {
-    const recipeId = Number(request.params.id);
-    const getRecipe = recipes.find((recipe) => recipe.id === recipeId);
 
-    if (!getRecipe) {
-        response.status(500).send('Recipe not found.')
-    } else {
-        response.json(getRecipe);
-    }
-});
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+// app.get('/recipes', (request, response) => {
+//     response.json(recipes);
+// });
+
+// app.get('/recipes/:id', (request, response) => {
+//     const recipeId = Number(request.params.id);
+//     const getRecipe = recipes.find((recipe) => recipe.id === recipeId);
+
+//     if (!getRecipe) {
+//         response.status(500).send('Recipe not found.')
+//     } else {
+//         response.json(getRecipe);
+//     }
+// });
+
+// app.listen(3000, () => {
+//     console.log("Server running on port 3000");
+// });
